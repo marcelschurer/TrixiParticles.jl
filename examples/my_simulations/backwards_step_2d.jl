863352580f1bbb62dbdec11f366d5915203e5cea
marcelschurer <marcel.schurer@outlook.de>
2024-07-04 11:07:38 +0200

    finished backwards step

--- examples/my_simulations/backwards_step_2d.jl
+++ examples/my_simulations/backwards_step_2d.jl
@@ -1,42 +1,44 @@
-# 2D channel flow simulation with open boundaries.
+# 2D backwards step simulation following:
+# http://dx.doi.org/10.1017/S0022112083002839
+# https://doi.org/10.1016/j.icheatmasstransfer.2011.07.003
 
 using TrixiParticles
 using OrdinaryDiffEq
 
 # ==========================================================================================
 # ==== Resolution
-particle_spacing = 0.1 # following https://doi.org/10.1016/j.cma.2020.113119
+particle_spacing = 0.1
+smoothing_length = 3 * particle_spacing
 
 # Make sure that the kernel support of fluid particles at a boundary is always fully sampled
 boundary_layers = 5
 
-# Make sure that the kernel support of fluid particles at an open boundary is always
-# fully sampled.
-# Note: Due to the dynamics at the inlets and outlets of open boundaries,
-# it is recommended to use `open_boundary_layers > boundary_layers`
-open_boundary_layers = 6 # following https://doi.org/10.1016/j.cma.2020.113119
+# It is recommended to use `open_boundary_layers > boundary_layers`
+open_boundary_layers = 6
 
 # ==========================================================================================
 # ==== Experiment Setup
-tspan = (0.0, 10.0)
+tspan = (0.0, 15.0)
 flow_direction = [1.0, 0.0]
-const prescribed_velocity = 1.0 # following https://doi.org/10.1016/j.cma.2020.113119 
-sound_speed = 10 * prescribed_velocity # following https://doi.org/10.1016/j.cma.2020.113119 
 
-# length of domain and initial fluid particle positions
-L = 10 # following http://dx.doi.org/10.1017/S0022112083002839 (200mm/500mm)
-h = 4.9 # Stepsize following http://dx.doi.org/10.1017/S0022112083002839
-reynolds_number = 389 # following https://doi.org/10.1016/j.cma.2020.113119
-fluid_density = 997.0 # following https://doi.org/10.1016/j.cma.2020.113119
-#kinematic_viscosity = 4 * h / (3 * reynolds_number * prescribed_velocity) # following https://doi.org/10.1016/j.cma.2020.113119
+# note that you might have to change velocity_function_outlet(pos, t) for another prescribed_velocity
+const prescribed_velocity = 1.0
+sound_speed = 10 * prescribed_velocity
+
+reynolds_number = 389 # laminar flow
+fluid_density = 997.0 # water at 20°C
 kinematic_viscosity = 0.001 # water at 20°C
 
 # For this particular example, it is necessary to have a background pressure.
 # Otherwise the suction at the outflow is to big and the simulation becomes unstable.
-pressure = 1.0 # 1.0 following https://doi.org/10.1016/j.cma.2020.113119
+pressure = 101325 - 0.5 * fluid_density * prescribed_velocity^2 # Bernoulli's equation
 
-state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
-                                   exponent=7, background_pressure=pressure)
+L = 10 # length of domain [L]
+slip_wall_length = 2 * open_boundary_layers # must be < 0.25*L/particle_spacing
+h = 4.9 # height of the step following the experiment
+
+# ==========================================================================================
+# ==== Initial Conditions
 
 fluid_size_inlet = (Int(floor(0.25 * L / particle_spacing)),
                     Int(floor(5.2 / particle_spacing)))
@@ -56,23 +58,27 @@ ic_fluid = union(fluid_inlet, fluid_outlet)
 
 boundary_top = RectangularShape(particle_spacing,
                                 ((fluid_size_inlet[1] + fluid_size_outlet[1] +
-                                  2 * open_boundary_layers),
+                                  2 * open_boundary_layers - 2 * slip_wall_length),
                                  boundary_layers),
-                                (-open_boundary_layers * particle_spacing,
+                                ((slip_wall_length - open_boundary_layers) *
+                                 particle_spacing,
                                  fluid_size_inlet[2] * particle_spacing),
                                 pressure=pressure,
                                 density=fluid_density)
 
 boundary_bottom_left = RectangularShape(particle_spacing,
-                                        (fluid_size_inlet[1] + open_boundary_layers,
+                                        (fluid_size_inlet[1] + open_boundary_layers -
+                                         slip_wall_length,
                                          boundary_layers),
-                                        (-open_boundary_layers * particle_spacing,
+                                        ((slip_wall_length - open_boundary_layers) *
+                                         particle_spacing,
                                          -boundary_layers * particle_spacing),
                                         pressure=pressure,
                                         density=fluid_density)
 
 boundary_bottom_right = RectangularShape(particle_spacing,
-                                         (fluid_size_outlet[1] + open_boundary_layers,
+                                         (fluid_size_outlet[1] + open_boundary_layers -
+                                          slip_wall_length,
                                           boundary_layers),
                                          (fluid_size_inlet[1] * particle_spacing,
                                           -(fluid_size_outlet[2] - fluid_size_inlet[2] +
@@ -94,10 +100,49 @@ boundary_left = RectangularShape(particle_spacing,
 ic_boundary = union(boundary_top, boundary_bottom_left, boundary_bottom_right,
                     boundary_left)
 
+slip_wall_top_left = RectangularShape(particle_spacing,
+                                      (slip_wall_length,
+                                       boundary_layers),
+                                      ((-open_boundary_layers) * particle_spacing,
+                                       fluid_size_inlet[2] * particle_spacing),
+                                      pressure=pressure,
+                                      density=fluid_density)
+
+slip_wall_bottom_left = RectangularShape(particle_spacing,
+                                         (slip_wall_length,
+                                          boundary_layers),
+                                         ((-open_boundary_layers) * particle_spacing,
+                                          -boundary_layers * particle_spacing),
+                                         pressure=pressure,
+                                         density=fluid_density)
+
+slip_wall_top_right = RectangularShape(particle_spacing,
+                                       (slip_wall_length,
+                                        boundary_layers),
+                                       ((open_boundary_layers + fluid_size_inlet[1] +
+                                         fluid_size_outlet[1] -
+                                         slip_wall_length) * particle_spacing,
+                                        fluid_size_inlet[2] * particle_spacing),
+                                       pressure=pressure,
+                                       density=fluid_density)
+
+slip_wall_bottom_right = RectangularShape(particle_spacing,
+                                          (slip_wall_length,
+                                           boundary_layers),
+                                          ((open_boundary_layers + fluid_size_inlet[1] +
+                                            fluid_size_outlet[1] -
+                                            slip_wall_length) * particle_spacing,
+                                           -(fluid_size_outlet[2] - fluid_size_inlet[2] +
+                                             boundary_layers) * particle_spacing),
+                                          pressure=pressure,
+                                          density=fluid_density)
+
+ic_slip_wall = union(slip_wall_top_left, slip_wall_bottom_left, slip_wall_top_right,
+                     slip_wall_bottom_right)
+
 # ==========================================================================================
 # ==== Fluid
-smoothing_length = 3 * particle_spacing
-smoothing_kernel = WendlandC2Kernel{2}() # SchoenbergQuinticSplineKernel/WendlandC2Kernel following https://doi.org/10.1016/j.cma.2020.113119
+smoothing_kernel = WendlandC2Kernel{2}()
 
 fluid_density_calculator = ContinuityDensity()
 
@@ -108,35 +153,21 @@ viscosity = ViscosityAdami(nu=kinematic_viscosity)
 fluid_system = EntropicallyDampedSPHSystem(ic_fluid, smoothing_kernel,
                                            smoothing_length,
                                            sound_speed, viscosity=viscosity,
+                                           transport_velocity=TransportVelocityAdami(pressure),
                                            density_calculator=fluid_density_calculator,
                                            buffer_size=n_buffer_particles)
-
-#Alternatively the WCSPH scheme can be used
-# alpha = 8 * kinematic_viscosity / (smoothing_length * sound_speed)
-# viscosity = ArtificialViscosityMonaghan(; alpha, beta=0.0)
-# density_diffusion = DensityDiffusionAntuono(ic_fluid; delta=0.1)
-
-# fluid_system = WeaklyCompressibleSPHSystem(ic_fluid, fluid_density_calculator,
-#                                            state_equation, smoothing_kernel,
-#                                            smoothing_length, viscosity=viscosity,
-#                                            buffer_size=n_buffer_particles,
-#                                            density_diffusion=density_diffusion)
 
 # ==========================================================================================
 # ==== Open Boundary
 function velocity_function_inlet(pos, t)
-    # Use this for a time-dependent inflow velocity
-    # return SVector(0.5prescribed_velocity * sin(2pi * t) + prescribed_velocity, 0)
-
     return SVector(prescribed_velocity, 0.0)
 end
 
 function velocity_function_outlet(pos, t)
-    # Use this for a time-dependent inflow velocity
-    #return SVector(0.5prescribed_velocity * sin(2pi * t) + prescribed_velocity, 0)
+
+    # time dependent velocity profile at the outlet for stability
+    # f(t) is designed for prescribed_velocity = 1.0
     return SVector(0.15(1 + tanh(5(t - 0.9))), 0)
-    # return SVector((5.2 / (4.9 + 5.2)) * prescribed_velocity, 0.0)
-    #return SVector((0.0, 0.0))
 end
 
 inflow = InFlow(; plane=([0.0, 0.0], [0.0, fluid_size_inlet[2] * particle_spacing]),
@@ -174,22 +205,30 @@ open_boundary_out = OpenBoundarySPHSystem(outflow; sound_speed, fluid_system,
 boundary_model = BoundaryModelDummyParticles(ic_boundary.density,
                                              ic_boundary.mass,
                                              AdamiPressureExtrapolation(),
-                                             #state_equation=state_equation,
                                              viscosity=viscosity,
                                              smoothing_kernel, smoothing_length)
 
 boundary_system = BoundarySPHSystem(ic_boundary, boundary_model)
 
+slip_wall_model = BoundaryModelDummyParticles(ic_boundary.density,
+                                              ic_boundary.mass,
+                                              AdamiPressureExtrapolation(),
+                                              viscosity=nothing, # slip wall condition
+                                              smoothing_kernel, smoothing_length)
+
+slip_wall_system = BoundarySPHSystem(ic_slip_wall, slip_wall_model)
+
 # ==========================================================================================
 # ==== Simulation
 semi = Semidiscretization(fluid_system, open_boundary_in, open_boundary_out,
-                          boundary_system)
+                          boundary_system, slip_wall_system)
 
 ode = semidiscretize(semi, tspan)
 
 info_callback = InfoCallback(interval=50)
 saving_callback = SolutionSavingCallback(dt=0.02, prefix="",
-                                         output_directory="out_my_simulation")
+                                         output_directory="out_backwards_step_2d")
+#output_directory="out_my_simulation")
 
 callbacks = CallbackSet(info_callback, saving_callback, UpdateCallback())
 
